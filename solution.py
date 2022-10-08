0e647c9f89c5039dd3010bae84bd5b387554ad58
Zehra Kaya <72206348+thezkaya@users.noreply.github.com>
2022-10-08 05:51:00 +0300

    not sure

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 
-multiples = [ numbers for numbers in range(1,1000) if numbers % 3 == 0 ] # return numbers that are multiples of 3 below 1000.
-total = [ numbers for numbers in range(1,1000) if (numbers % 5 == 0 and numbers % 3 != 0)] # return numbers that are multiples of 5 but not of 3 below 1000.
+multiples = [ x for x in range(1,1000) if x % 3 == 0 ] # return numbers that are multiples of 3 below 1000.
+total = [ y for y in range(1,1000) if (y % 5 == 0 and y % 3 != 0)] # return numbers that are multiples of 5 but not of 3 below 1000.
 multiples.extend(total) 
 sum(multiples)
 
